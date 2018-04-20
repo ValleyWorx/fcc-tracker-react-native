405666712f650f0d7db1925e1ba1baf27b022d1b
Brayanmcz <brayan@gmail.com>
2018-04-20 15:56:28 -0700

    Registration page styling done

--- src/screens/auth/registration.js
+++ src/screens/auth/registration.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, Button, TouchableOpacity } from 'react-native';
 import { Header } from '../../components/header';
 
 export class Registration extends React.Component {
@@ -7,7 +7,8 @@ export class Registration extends React.Component {
     super(props);
     this.newUser = {
       email: 'Email',
-      password: 'Password'
+      password: 'Password',
+      fccCode: 'FCC Code'
      };
   }
 
@@ -15,8 +16,12 @@ export class Registration extends React.Component {
     return (
       <View style={styles.container}>
         <Header></Header>
-        <Text>FCC Tracker!</Text>
 
+        <View style={styles.backgroundContainer}>
+        <Image style={styles.backgroundImage} source={require('./../../../assets/img/fcc.png')}></Image>
+
+        <View style={styles.formContainer}>
+        <Text style={styles.text}>Activity Tracker!</Text>
         <TextInput
         style={styles.textInput}
         onChangeText={(text) => this.setState({text})}
@@ -27,6 +32,22 @@ export class Registration extends React.Component {
         onChangeText={(text) => this.setState({text})}
         value={this.newUser.password}
         />
+        <TextInput
+        style={styles.textInput}
+        onChangeText={(text) => this.setState({text})}
+        value={this.newUser.fccCode}
+        />
+
+        <TouchableOpacity style={styles.SignUpButton}
+          onPress={test => {
+            console.log(test);
+          }}
+        ><Text>Sign Up</Text></TouchableOpacity>
+
+        </View>
+        </View>
+
+
       </View>
     );
   }
@@ -35,13 +56,48 @@ export class Registration extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+  },
+  backgroundContainer: {
+    flex: 1,
+  },
+  formContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   textInput: {
     height: 40,
     width: 200,
-    padding: 10,
+    marginVertical: 5,
     borderColor: 'gray',
-    borderWidth: 1
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    borderColor: '#006400',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingLeft: 5
+  },
+  text: {
+    marginBottom: 50,
+    fontWeight: 'bold',
+    fontSize: 33,
+    color: '#006400'
+  },
+  backgroundImage: {
+    flex: 1,
+    position: 'absolute',
+    resizeMode: 'cover',
+    width: '100%',
+    height: '100%'
+  },
+  SignUpButton: {
+    backgroundColor: '#FF9C2A',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginTop: 10,
+    borderColor: '#006400',
+    borderWidth: 1,
+    borderRadius: 5
   }
 });
