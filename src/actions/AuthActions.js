324acc8afb8a79f1b7da089e1c9f99c8a0c479b6
Peter Vann <petervann@yahoo.com>
2018-05-18 12:25:10 -0700

    fixed auth

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -13,26 +13,23 @@ export const logIn = (loginObject, navigation) => {
     return async (dispatch) => {
         dispatch({type: AUTH_LOADING, payload: true});
         const {email, password} = loginObject;
-        const response = await api({
-            endpoint: 'auth',
-            method: 'POST', 
-            body: {
-                email: email,
-                password: password,
-            }
-        });
-        if (response.ok) {
-            response.json().then(async (respJson) => {
-                console.log('respJson', respJson);
-                const {jwt, refreshToken} = respJson;
-                await AsyncStorage.setItem('jwt', jwt);
-                await AsyncStorage.setItem('refreshToken', refreshToken);
-                dispatch({type: LOG_IN_SUCCESS, payload: respJson});
-                dispatch({type: AUTH_LOADING, payload: false});
-                navigation.navigate('HomeScreen');
-            })
-        } else {
-            console.log('this has failed', response);
+        try {
+            const response = await api({
+                endpoint: 'auth',
+                method: 'POST', 
+                skipAuth: true,
+                body: {
+                    email: email,
+                    password: password,
+                }
+            });
+            const {jwt, refreshToken} = response;
+            await AsyncStorage.setItem('jwt', jwt);
+            await AsyncStorage.setItem('refreshToken', refreshToken);
+            dispatch({type: LOG_IN_SUCCESS, payload: response});
+            dispatch({type: AUTH_LOADING, payload: false});
+            navigation.navigate('HomeScreen');
+        } catch(e) {
             dispatch({type: LOG_IN_FAIL});
             dispatch({type: AUTH_LOADING, payload: false});
         }
@@ -45,11 +42,6 @@ export const scrape = () => {
             endpoint: 'user/scrape',
             method: 'GET',
         });
-        console.log('response', response);
-        if (response.ok) {
-            response.json().then(respJson => {
-                console.log('scrape respJson', respJson);
-            })
-        }
+        console.log('scrape results: ', response);
     }
 }
--- src/api/index.js
+++ src/api/index.js
@@ -2,21 +2,27 @@ import {checkTokens} from '../actions/Helpers';
 
 export const apiURL = 'https://api.fcctracker.com/';     //change url to AWS
 
-export const api = ({endpoint, method, body}) => {
-    return checkTokens().then(async jwt => {
-        const headers = {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        };
+export const api = async ({endpoint, method, skipAuth, body}) => {
+    const headers = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    };
+    
+    if (!skipAuth) {
+        const jwt = await checkTokens();
         if (jwt) {
             headers['Authorization'] = `Bearer ${jwt}`;
         }
-        const response = await fetch(apiURL + endpoint, {
-            method,
-            headers: headers,
-            body: JSON.stringify(body)
-        });
-        return response;
-    })
+    }
 
+    const response = await fetch(apiURL + endpoint, {
+        method,
+        headers: headers,
+        body: JSON.stringify(body)
+    });
+    if (response.ok) {
+        return await response.json();
+    } else {
+        throw new Error('Api Error');
+    }
 };
