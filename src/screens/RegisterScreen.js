7592540e08e7fbb0189924b27679149f401b2551
Peter Vann <petervann@yahoo.com>
2018-06-29 13:24:35 -0700

    wired-up registration

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { register } from "../actions";
+import {connect} from 'react-redux';
+import {register} from "../actions";
 import {
   StyleSheet,
   Text,
@@ -12,8 +12,8 @@ import {
 } from "react-native";
 import Header from "../components/Header";
 import FccButton from "../components/fcc-button";
+import { FCCSpinner } from "../components/FCCSpinner";
 import * as STYLES from '../styles';
-
 
 class RegisterScreen extends React.Component {
   constructor(props) {
@@ -25,78 +25,86 @@ class RegisterScreen extends React.Component {
       password: "",
       fccCode: ""
     };
-    this.vOffset = Platform.OS === "ios" ? 100 : 10; //Size of vertical offset
+    this.vOffset = Platform.OS === "ios"
+      ? 100
+      : 10; //Size of vertical offset
   }
-	
-  onLoginPress = async () => {
-    const loginObject = {
-        fname: this.state.fname, 
+
+  onRegisterPress = async () => {
+    const registerObject = {
+        fname: this.state.fname,
         lname: this.state.lname,
         email: this.state.email,
         password: this.state.password,
         fccCode: this.state.fccCode
     }
-    this.props.register(loginObject, this.props.navigation);
+    this.props.register(registerObject, this.props.navigation);
 }
+
+  renderButton() {
+    if (this.props.loading) {
+      return (<FCCSpinner/>);
+    }
+    return (<FccButton buttonText={"Sign Up"} onPress={this.onRegisterPress}/>)
+  }
+
+  renderError = () => {
+    if (this.props.errorMsg) {
+      return (
+        <Text style={styles.errorMsg}>{this.props.errorMsg}</Text>
+      )
+    }
+  }
 
   render() {
     return (
       <View style={styles.container}>
-			<Header
-        leftType={'back'}
-        centerType={'logo'}
-			/>
+        <Header leftType={'back'} centerType={'logo'}/>
 
         <View style={styles.backgroundContainer}>
           <Image
             style={styles.backgroundImage}
-            source={require("../../assets/img/fcc.png")}
-          />
-          {/* Could wrap in scroll View */}
-          <KeyboardAvoidingView
-            style={styles.formContainer}
-            behavior="padding"
-            keyboardVerticalOffset={this.vOffset} // Gets computed when changes props or state
-            enabled
-          >
+            source={require("../../assets/img/fcc.png")}/> {/* Could wrap in scroll View */}
+          <KeyboardAvoidingView style={styles.formContainer} behavior="padding" keyboardVerticalOffset={this.vOffset} // Gets computed when changes props or state
+            enabled>
             <Text style={styles.text}>Activity Tracker!</Text>
             <TextInput
               style={styles.textInput}
-              onChangeText={text => this.setState({ text })}
+              onChangeText={text => this.setState({text})}
               value={this.state.fname}
               placeholder={"First Name"}
-              underlineColorAndroid={'transparent'}
-            />
+              underlineColorAndroid={'transparent'}/>
             <TextInput
               style={styles.textInput}
-              onChangeText={text => this.setState({ text })}
+              onChangeText={text => this.setState({text})}
               value={this.state.lname}
               placeholder={"Last Name"}
-              underlineColorAndroid={'transparent'}
-            />
+              underlineColorAndroid={'transparent'}/>
             <TextInput
               style={styles.textInput}
-              onChangeText={text => this.setState({ text })}
+              onChangeText={text => this.setState({text})}
               value={this.state.email}
               placeholder={"Email"}
-              underlineColorAndroid={'transparent'}
-            />
+              underlineColorAndroid={'transparent'}/>
             <TextInput
               style={styles.textInput}
-              onChangeText={text => this.setState({ text })}
+              onChangeText={text => this.setState({text})}
               value={this.state.password}
               secureTextEntry={true}
               placeholder={"Password"}
-              underlineColorAndroid={'transparent'}
-            />
+              underlineColorAndroid={'transparent'}/>
             <TextInput
               style={styles.textInput}
-              onChangeText={text => this.setState({ text })}
+              onChangeText={text => this.setState({text})}
               value={this.state.fccCode}
               placeholder={"FCC Code"}
-              underlineColorAndroid={'transparent'}
-            />
-            <FccButton buttonText={"Sign Up"} onPress={this.onLoginPress} />
+              underlineColorAndroid={'transparent'}/>
+            <View style={styles.errorContainer}>
+                {this.renderError()}
+            </View>
+            <View style={styles.buttonContainer}>
+                {this.renderButton()}
+            </View>
           </KeyboardAvoidingView>
         </View>
       </View>
@@ -138,6 +146,17 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     width: "100%",
     height: "100%"
+  },
+  errorMsg: {
+    color: '#a00',
+  },
+  buttonContainer: {
+      alignItems: 'center',
+  },
+  errorContainer: {
+      height: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
   }
 });
 
